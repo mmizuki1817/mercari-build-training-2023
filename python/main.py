c5b2250d383efd369d14e70c1d165bc4bf228731
mmizuki <mmizuki1817@gmail.com>
2023-05-19 10:53:44 +0900

    Revert "change the path of requirements.txt. git rm main.py and pycache"
    This reverts commit a53d1e6588f71789d07019a458ff45e3562e52a7.

---
+++ python/main.py
@@ -0,0 +1,105 @@
+import hashlib
+import json
+import os
+import os.path
+import logging
+import pathlib
+from fastapi import FastAPI, Form, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+
+app = FastAPI()
+logger = logging.getLogger("uvicorn")
+logger.level = logging.INFO
+images = pathlib.Path(__file__).parent.resolve() / "images"
+origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET","POST","PUT","DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Hello, world!"}
+
+@app.post("/items")
+def add_item(name: str = Form(...), category: str = Form(...), image: str = Form(...)):
+    logger.info(f"Receive item: {name}")
+    
+    # rename image to hash
+    img = image
+    if os.path.isfile(img) == True:
+        copy = 'copy.jpg'
+        shutil.copyfile(img, copy)
+        with open(copy, 'rb') as copy_f:    
+            sha256 = hashlib.sha256(copy_f.read()).hexdigest()
+            ext = ".jpg"
+            hash_img = sha256 + ext
+            os.rename(copy, f"./images/{hash_img}")
+    # Error handling (no image)
+    else:
+        logger.info("Image not found")
+        hash_img = "..."
+
+    # add log to a json file
+    log = { "items" : 
+            [{"name" : f"{name}",
+             "category": f"{category}",
+             "image_filename" : f"{hash_img}"
+            }]}
+    
+    # Error handling(no file)
+    if os.path.isfile('items.json') == False:
+        with open('items.json', 'w+') as f:
+            json.dump(log, f)
+    # Error handling (empty json file)
+    elif os.path.getsize('items.json') == 0:
+        with open('items.json', 'w') as f:
+            json.dump(log, f)
+    else:
+        with open('items.json', 'r') as f:
+            read_data = json.load(f)
+            read_data = read_data["items"]
+            read_data.append({"name" : f"{name}", "category": f"{category}", "image_filename" : f"{hash_img}"})
+            log = {"items": read_data}
+            with open('items.json', 'w') as f:
+                json.dump(log, f)        
+
+    return ({"message": f"item received: {name}"})
+
+@app.get("/items")
+def show_list_of_items():
+    try:
+        with open('items.json', 'r') as f:
+            return (json.load(f))
+    except FileNotFoundError:
+        return ("no items")
+
+@app.get("/items/{item_id}")
+def show_detail_of_item(item_id):
+    with open('items.json', 'r') as f:
+        jsn = []
+        jsn = json.load(f)
+        # Error handling (invalid item_id)
+        if item_id.isnumeric()==False or int(item_id) < 1 or int(item_id) > len(jsn):
+            return("invalid id")
+        return(jsn[int(item_id) - 1])
+
+#Optional課題、未着手
+@app.get("/image/{image_filename}")
+async def get_image(image_filename):
+    # Create image path
+    image = images / image_filename
+
+    if not image_filename.endswith(".jpg"):
+        raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
+
+    if not image.exists():
+        logger.debug(f"Image not found: {image}")
+        image = images / "default.jpg"
+
+    return FileResponse(image)
